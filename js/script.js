7e6e8a12d63b142a69305afb57f8d667934b19b6
Eric <46570378+ebrown48@users.noreply.github.com>
2023-03-15 23:50:26 -0400

    add parameters for valid guesses

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersAppear = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const inputBox = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word ="magnolia";
+const guessedLetters = [];
 
 const blackSpot = function (word) {
     const hiddenLetters = [];
@@ -22,7 +23,38 @@ blackSpot(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
     const guess = inputBox.value;
-    console.log(guess);
+    const goodGuess = letterValidator(guess);
+    if (goodGuess) {
+        makeGuess(guess);
+    }
     inputBox.value = "";
 });
+
+const letterValidator = function (input) {
+    const acceptedLetter = /[a=zA-Z]/;
+    if (input.length ===0) {
+        //is the input empty?
+        message.innerText = "please enter a letter.";
+    } else if (input.length > 1) {
+        //is the input more than one letter?
+        message.innerText = "please enter only one letter.";
+    } else if (!input.match(acceptedLetter)) {
+        //did they type something other than a letter?
+        message.innerText = "Only use letters not munbers or special characters."; 
+    }   else {
+        //recieved a valid answer.
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You already guessed that letter. Guess another.";
+    } else {
+       guessedLetters.push(guess);
+       console.log(guessedLetters);
+    }
+};
