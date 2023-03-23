3ef9aa154d4570be69b58ce86d8217e3cce524ea
Eric <46570378+ebrown48@users.noreply.github.com>
2023-03-23 19:35:47 -0400

    fetch words and remaining guesses

--- js/script.js
+++ js/script.js
@@ -2,19 +2,32 @@ const guessedLettersAppear = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const inputBox = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
-const remainingGuesses = document.querySelector(".remaining");
+const remainingGuessesElement = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
-const word ="magnolia";
+let word ="magnolia";
 const guessedLetters = [];
+let remainingGuesses = 8;
+
+ const getWord = async function () {
+     const response = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+     const words = await response.text();
+     const wordArray = words.split("\n");
+     const randomIndex = Math.floor(Math.random() * wordArray.length);
+     word = wordArray[randomIndex].trim();
+    //console.log(wordArray);
+    blackSpot(word);
+ };
+
+ getWord();
 
 // show symbol for chosen word's letters
 const blackSpot = function (word) {
     const hiddenLetters = [];
     for (const letter of word) {
-        console.log(letter);
+        //console.log(letter);
         hiddenLetters.push("●");
     }
     wordInProgress.innerText = hiddenLetters.join("");
@@ -58,6 +71,7 @@ const makeGuess = function (guess) {
     } else {
        guessedLetters.push(guess);
        console.log(guessedLetters);
+        showGuessCount (guess);
         currentGuesses();
         updateWordProgress(guessedLetters);
     }
@@ -86,6 +100,24 @@ const updateWordProgress = function (guessedLetters) {
     }
     wordInProgress.innerText = revealWord.join("");
     youWin();
+};
+
+const showGuessCount = function (guess) {
+    const makeUpper = word.toUpperCase();
+    if (!makeUpper.includes(guess)) {
+        message.innerText = `Sorry, the word does not have ${guess} in it.`;
+        remainingGuesses -= 1;
+    } else {
+        message.innerText = ` ${guess} is the correct guess.`
+    }
+
+    if (remainingGuesses === 0) {
+        message.innerHTML = "You have run out of guesses. Game over."
+    } else if (remainingGuesses === 1) {
+        remainingGuessesSpan.innerText = `${remainingGuesses} guess`;
+    } else {
+        remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
+    }
 };
 
 const youWin = function () {
